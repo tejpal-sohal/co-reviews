27ef5bba92abb3b9b9df5ebf09994d09bd5b443f
tejpal-sohal <tejpal54@gmail.com>
2020-09-19 15:37:13 +0100

    added build folder

---
+++ build/precache-manifest.9204caa32ab9f639bf26e6bf4f3ea41e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd510ce5e4890ef0d0f09a6e8e7967c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c66d7ed6f905088e29fa",
+    "url": "/static/css/main.cde10eb7.chunk.css"
+  },
+  {
+    "revision": "9a62d988e093546cff6e",
+    "url": "/static/js/2.0436e22f.chunk.js"
+  },
+  {
+    "revision": "b866bd97309441e8a83c439a35559cce",
+    "url": "/static/js/2.0436e22f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c66d7ed6f905088e29fa",
+    "url": "/static/js/main.0c5f1871.chunk.js"
+  },
+  {
+    "revision": "762eec3fc461a456eed1",
+    "url": "/static/js/runtime-main.3697f3dc.js"
+  },
+  {
+    "revision": "1cbf334ff757c3cbd3336fcbc21e4a0c",
+    "url": "/static/media/logo.1cbf334f.svg"
+  }
+]);
